2d82a522364dc4dd53741fb9e485c3a1daedeeb8
Jacek Kunicki <jacek@kunicki.org>
2015-04-20 15:06:44 +0200

    Controller skeleton plus routes

---
+++ app/controllers/Conference.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+object Conference extends Controller {
+
+  def getRooms = Action {
+    Ok
+  }
+
+  def addRoom = Action {
+    Ok
+  }
+}
