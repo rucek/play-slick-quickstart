811fb576445a4a8fbe74700fcc4e9bbce6506593
Jacek Kunicki <jacek@kunicki.org>
2015-04-20 15:13:13 +0200

    Room actions in controller with missing JSON support

--- app/controllers/Conference.scala
+++ app/controllers/Conference.scala
@@ -1,14 +1,25 @@
 package controllers
 
+import db.RoomsDao
+import models.Room
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc.{Action, Controller}
 
 object Conference extends Controller {
 
+  val roomsDao = new RoomsDao
+
   def getRooms = Action {
-    Ok
+    Ok(Json.toJson(roomsDao.findAll))
   }
 
-  def addRoom = Action {
-    Ok
+  def addRoom = Action(parse.json) { request =>
+    request.body.validate[Room].fold(
+      errors => BadRequest(JsError.toFlatJson(errors)),
+      room => {
+        val id = roomsDao.create(room)
+        Created(Json.obj("id" -> id))
+      }
+    )
   }
 }
